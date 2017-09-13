c9ccb4fb40c69e62654dd3d9bcea547c693e5205
Kunal Verma <kunalverma.ce@gmail.com>
2017-09-13 12:13:44 +0530

    code changes.

--- AmazonSDK/Program.cs
+++ AmazonSDK/Program.cs
@@ -26,14 +26,14 @@ namespace AmazonSDK
     {
         static void Main(string[] args)
         {
-            //while (true)
-            //{
-            //    Console.WriteLine("Start: " + DateTime.Now.ToString("MM_dd_yyyy_HH_mm_ss"));
-            //    Thread t = new Thread(Scheduler);
-            //    t.Start();
-            //    Thread.Sleep(500);
-            //}
-            Scheduler();
+            while (true)
+            {
+                Console.WriteLine("Start: " + DateTime.Now.ToString("MM_dd_yyyy_HH_mm_ss"));
+                Thread t = new Thread(Scheduler);
+                t.Start();
+                Thread.Sleep(500);
+            }
+            //Scheduler();
         }
 
         private static void Scheduler()
