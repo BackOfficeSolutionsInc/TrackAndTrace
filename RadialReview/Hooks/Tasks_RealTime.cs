282e2aa72a3caf63cd91d18eb3f1954cd37e4976
lbush <lynneabush@gmail.com>
2017-09-19 12:41:39 -0700

    ITaskHook

---
+++ RadialReview/Hooks/Tasks_RealTime.cs
@@ -0,0 +1,27 @@
+﻿using RadialReview.Utilities.Hooks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NHibernate;
+using System.Threading.Tasks;
+
+namespace RadialReview.Hooks {
+    public class Tasks_RealTime : ITaskHook {
+        public Task ClaimTask(ISession s) {
+            throw new NotImplementedException();
+        }
+
+        public Task CompleteTask(ISession s) {
+            throw new NotImplementedException();
+        }
+
+        public Task SyncTask(ISession s) {
+            throw new NotImplementedException();
+        }
+
+        public Task UnclaimTask(ISession s) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RadialReview/Utilities/Hooks/ITaskHook.cs
@@ -0,0 +1,15 @@
+﻿using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadialReview.Utilities.Hooks {
+    public interface ITaskHook : IHook {
+        Task ClaimTask(ISession s);
+        Task UnclaimTask(ISession s);
+        Task SyncTask(ISession s);
+        Task CompleteTask(ISession s);
+    }
+}
