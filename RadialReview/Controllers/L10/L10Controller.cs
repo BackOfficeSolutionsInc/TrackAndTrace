bfacd4b88a7b739777f9711dc71956f195d49b6a
Clay Upton <clay.upton@mytractiontools.com>
2018-02-02 18:44:06 -0800

    fixed adding rocks, measurables to meeting

--- RadialReview/Controllers/L10/L10Controller.cs
+++ RadialReview/Controllers/L10/L10Controller.cs
@@ -446,7 +446,7 @@ namespace RadialReview.Controllers {
 				var createdUser = result.Item2;
 				try {
 					await L10Accessor.AddAttendee(GetUser(), meetingId, createdUser.Id);
-				} catch (Exception) {
+				} catch (Exception e) {
 					throw new PermissionsException("Could not add to meeting.");
 				}
 			} else {
@@ -466,7 +466,7 @@ namespace RadialReview.Controllers {
 				var result = await ScorecardAccessor.CreateMeasurable(GetUser(), builder);
 				try {
 					await L10Accessor.AttachMeasurable(GetUser(), meetingId, result.Id);
-				} catch (Exception) {
+				} catch (Exception e) {
 					throw new PermissionsException("Could not add to meeting.");
 				}
 			} else {
@@ -486,7 +486,7 @@ namespace RadialReview.Controllers {
 				var result = await RockAccessor.CreateRock(GetUser(), o.AccountableUser, o.Title);
 				try {
 					await L10Accessor.AttachRock(GetUser(), meetingId, result.Id, o.AddToVTO);
-				} catch (Exception) {
+				} catch (Exception e) {
 					throw new PermissionsException("Could not add to meeting.");
 				}
 			} else {
