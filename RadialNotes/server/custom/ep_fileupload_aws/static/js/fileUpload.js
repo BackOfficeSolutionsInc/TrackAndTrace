876e95ab792f7ef21970b1992360a2c68d796610
Clay Upton <clay.upton@mytractiontools.com>
2018-04-18 03:24:34 -0700

    aws plugin working

--- RadialNotes/server/custom/ep_fileupload_aws/static/js/fileUpload.js
+++ RadialNotes/server/custom/ep_fileupload_aws/static/js/fileUpload.js
@@ -11,11 +11,11 @@ $(function(){
 
 		//console.log(""response)
         // result i.e. "/up/c79133b2c8a97533cc397f8d325ce17a.jpg", trimmed whitespace
-        var filePath = response.slice(response.indexOf("/up")).trim();
+        var filePath = response.slice(response.lastIndexOf("/")).trim();
         // "http://example.com/subfolder/p/padID" -> "http://example.com/subfolder"
         var etherpadRoot = document.location.href.slice(0, document.location.href.indexOf("/p/"));
         // "http://example.com/subfolder/up/c79133b2c8a97533cc397f8d325ce17a.jpg"
-        var fileUri = etherpadRoot + filePath;
+        var fileUri = etherpadRoot +"/up"+ filePath;
 
         // Puts the actual URL in the pad..
         padeditor.ace.replaceRange(undefined, undefined, " " + fileUri + " ");
