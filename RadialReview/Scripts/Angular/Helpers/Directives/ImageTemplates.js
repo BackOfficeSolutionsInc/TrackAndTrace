d79ea6fd6f35b3eb2da36a4b8188d5885523a28d
Kunal Verma <kunalverma.ce@gmail.com>
2018-05-25 16:56:45 +0530

    Added changes for owner name color icon for Todo's

--- RadialReview/Scripts/Angular/Helpers/Directives/ImageTemplates.js
+++ RadialReview/Scripts/Angular/Helpers/Directives/ImageTemplates.js
@@ -4,8 +4,8 @@
         scope: {
             user: "="
         },
-        link: function(scope, element, attrs) {
-        	var hash = 0, i, chr, len;        	
+        link: function (scope, element, attrs) {
+            var hash = 0, i, chr, len;
             if (scope.user && scope.user.Name) {
                 var str = scope.user.Name;
                 if (str.length != 0) {
@@ -18,15 +18,22 @@
                 hash = hash % 360;
                 scope.user.colorCode = hash;
 
-                if (typeof(scope.user.Initials)==="undefined") {
-                	scope.user.Initials=getInitials(scope.user.Name).toUpperCase();
+                if (typeof (scope.user.Initials) === "undefined") {
+                    scope.user.Initials = getInitials(scope.user.Name).toUpperCase();
                 }
             }
         },
-        template: "<span class='picture-container' title='{{user.Name}}'>" +
-			"<span ng-if='user.ImageUrl!=\"/i/userplaceholder\" && user.ImageUrl!=null && user.ImageUrl!=\"\"' class='picture' style='background: url({{user.ImageUrl}}) no-repeat center center; background-color:hsla({{::user.colorCode}}, 36%, 49%, 1);color:hsla({{::user.colorCode}}, 36%, 72%, 1)'></span>" +
-			"<span ng-if='user.ImageUrl==\"/i/userplaceholder\"' class='picture keep-background' style='background-color:hsla({{::user.colorCode}}, 36%, 49%, 1);color:hsla({{::user.colorCode}}, 36%, 72%, 1)'><span class='initials'>{{user.Initials}}</span></span>" +
-			"<span ng-if='user.ImageUrl==null || user.ImageUrl==\"\"' class='picture keep-background' style='color:#ccc'>n/a</span>" +
-			"</span>"
+        template: "<span class='picture-container' title='hsla({{::user.colorCode}}, 36%, 49%, 1) {{user.Name}} {{user.colorCode}}'>" +
+        "<span ng-if='user.ImageUrl!=\"/i/userplaceholder\" && user.ImageUrl!=null && user.ImageUrl!=\"\"' class='picture' style='background: url({{user.ImageUrl}}) no-repeat center center; background-color:hsla({{::user.colorCode}}, 36%, 49%, 1);color:hsla({{::user.colorCode}}, 36%, 72%, 1)'></span>" +
+        "<span ng-if='user.ImageUrl==\"/i/userplaceholder\"' class='picture keep-background' ng-style='getElementStyle(user.colorCode)'><span class='initials'>{{user.Initials}}</span></span>" +
+        "<span ng-if='user.ImageUrl==null || user.ImageUrl==\"\"' class='picture keep-background' style='color:#ccc'>n/a</span>" +
+        "</span>",
+        controller: function ($scope) {
+            $scope.getElementStyle = function (colorCode) {
+                return {
+                    'background-color': 'hsla(' + colorCode + ', 36%, 49%, 1)', 'color': 'hsla(' + colorCode + ', 36%, 72%, 1)'
+                };
+            };
+        }
     };
 });
