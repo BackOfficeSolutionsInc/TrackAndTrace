f1098dbc8e8b471203c4c6a6b57015992b590ba8
Kunal Verma <kunalverma.ce@gmail.com>
2018-06-07 16:58:05 +0530

    edit notes pop up fixes

--- RadialReview/Controllers/TodoController.cs
+++ RadialReview/Controllers/TodoController.cs
@@ -73,7 +73,7 @@ namespace RadialReview.Controllers {
 		}
 
 		[Access(AccessLevel.UserOrganization)]
-		public ActionResult EditModal(long id) {
+		public ActionResult EditModal(long id, bool showMeeting = true) {
 
 			if (id > 0) {
 				var todo = TodoAccessor.GetTodo(GetUser(), id);
@@ -84,6 +84,7 @@ namespace RadialReview.Controllers {
 			   .ToList();
 
 				ViewBag.Originating = "";
+				ViewBag.ShowMeeting = showMeeting;
 
 				meetings.Add(new MeetingVm() {
 					name = "Individual To-do List",
