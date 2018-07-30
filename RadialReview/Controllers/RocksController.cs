d651f86d80a035006a10441b339145be8b12cc5d
Kunal Verma <kunalverma.ce@gmail.com>
2018-07-30 19:38:57 +0530

    Added changes for due date displayfor Or Editorfor

--- RadialReview/Controllers/RocksController.cs
+++ RadialReview/Controllers/RocksController.cs
@@ -239,6 +239,7 @@ namespace RadialReview.Controllers {
 				Milestones = rockAndMs.Milestones.Select(x => new AngularMilestone(x)).ToList()
 			};
 
+			ViewBag.CanEdit = _PermissionsAccessor.IsPermitted(GetUser(), x => x.EditRock(id));
 			ViewBag.AnyL10sWithMilestones = rockAndMs.AnyMilestoneMeetings;
 
 			return PartialView(model);
