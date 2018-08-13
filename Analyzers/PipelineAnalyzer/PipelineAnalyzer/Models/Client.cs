a306b76cf529a7fbfc39b9efafba439e4f7f7eae
Clay Upton <clay.upton@mytractiontools.com>
2018-08-13 13:59:43 -0700

    created pipeline project

---
+++ Analyzers/PipelineAnalyzer/PipelineAnalyzer/Models/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PipelineAnalyzer.Models {
+
+	public enum Status {
+		Demo,
+		Trial,
+		Walkthrough,
+		Paying,
+		Lost,
+		Closed,
+
+
+
+	}
+
+	public class Client {
+		public string Name { get; set; }
+		public DateTime CreateTime { get; set; }
+	}
+}
