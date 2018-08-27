fdebd55674ed828d8201ddebe73b4a74384acac0
consmanzano <37304998+consmanzano@users.noreply.github.com>
2018-08-27 18:06:54 +0800

    FirePad Revision

--- RadialReview/Models/FirePad/FirePadData.cs
+++ RadialReview/Models/FirePad/FirePadData.cs
@@ -1,11 +1,9 @@
 ﻿namespace RadialReview.Models.FirePad
 {
-    public class FirePadData
-    {
+    public class FirePadData{
         public string padID { get; set; }
         public string text { get; set; }
         public string firepadRef { get; set; }
-        
     }
     
 }
